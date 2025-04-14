541056cccf1a91110ac9178743de743c4ce98873
Mallick <Goutam.Mallick@jacobs.com>
2025-04-14 18:14:22 +0530

    Adding tour guide sample

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,8 +7,44 @@ import { useEffect, useState } from "react";
 import { getAdminRoutes, getRoutes } from "@/utilities/routes/getRoute";
 import { Path } from "@/types/path.types";
 
+//import {TourGuideClient} from "@sjmc11/tourguidejs/src/Tour" // JS
+//import '@sjmcl1/tourguidejs/tourguide.css';
+
+//Dist way
+//import Tour from '../tour/tour.js';
+
 type User = {
   name: string;
+};
+
+const startTour = () => {
+  console.log("In Tour Guide");
+  /* const steps = [{
+
+    content: "This is a short guide to get you set up and show you where things are",
+    title: "Welcome aboard 👋",
+    target: "",
+    order: 1,
+    group: ""
+
+  }] 
+
+  const tg = new TourGuideClient({
+    steps: steps
+
+  }) */
+
+  /* const tg = new TourGuideClient({
+    exitOnClickOutside: false
+  }) */
+  
+  //tg.start();
+
+  //dist way
+  /* const tour = new Tour();
+  tour.init();
+  tour.start(); */
+  
 };
 
 export default function Header() {
@@ -18,12 +54,14 @@ export default function Header() {
   const [pages, setPages] = useState<Path[]>(
     user.isAdmin ? getRoutes().concat(getAdminRoutes()) : getRoutes()
   );
+
+  //data-tg-tour tag helps TourGuide to get nodes to display in tour
   return (
     <header className={"header top-bar"} id="header">
       <div className="header_toggle">
         <h2>Jacobs</h2>
       </div>
-      <div className="nav-menu">
+      <div className="nav-menu" data-tg-tour='<p>Nav Bar</p>' data-tg-order="0">
         {pages.map((e: Path, index) => (
           <Link
             href={e.url}
@@ -34,8 +72,10 @@ export default function Header() {
           </Link>
         ))}
       </div>
-
-      <div className="dropdown">
+      <div className="header_toggle" data-tg-tour='TourGuide' data-tg-order="2" onClick={startTour}>
+        <h2>Tour Guide</h2>
+      </div>
+      <div className="dropdown" data-tg-tour='Profile' data-tg-order="3">
         <div
           className="header_img float-end dropdown-toggle"
           id="dropdownMenuButton"
